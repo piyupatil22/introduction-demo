b5978ff766106422c47039b906ee656c1267ef25
priya <priya.o.patil@outlook.com>
2019-04-10 19:34:22 +0530

    initial commit

---
+++ src/introduction/Arrayexcercise.java
@@ -0,0 +1,62 @@
+package introduction;
+
+public class Arrayexcercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int x;
+		int y;
+		int a;
+		int a[x][y]= new int[2][3];
+		a[0][0]=2;
+		a[0][1]=4;
+		a[0][2]=5;*/
+		//int x;
+		//int y;
+		/*int a[][]={{2,4,5},{3,4,7}};
+		 
+		for(int i=0;i<a.length;i++) 
+		{
+			System.out.println(a[i]);
+		}
+		
+		
+		 
+		for(int j=0;j<a.length;j++) 
+		{
+			System.out.println(a[j]);
+		}*/
+		//to find minimum and maximum no.in the array,we have to assume the first value in array is min/max.
+		
+		
+		int abc[][]= {{2,3,4},{5,1,8},{7,9,10}};
+		int max=abc[0][0];
+		 
+		for(int i=0;i<3;i++)//for row 3*3
+		 {
+			 
+			for(int j=0;j<3;j++)//for coloumn 
+			 {
+				
+				if(abc[i][j]>max) //if(abc[i][j]<min)
+				{
+				
+					max=abc[i][j];	
+				}
+			 }
+		 }
+		 System.out.println(max);
+		 }
+		
+}	
+
+    /*  int abc[][]= {{2,3,4},{5,1,8}};
+      for(int i=0;i<3;i++)
+		 {
+			 for(int j=0;j<3;j++)
+			 {
+				 System.out.println(abc[i][j]);
+			 }
+			 }
+		 }*/
+		 
---
+++ src/introduction/Arraylistinterface.java
@@ -0,0 +1,22 @@
+package introduction;
+
+import java.util.ArrayList;
+
+public class Arraylistinterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  ArrayList<Integer> a=new ArrayList<Integer>();
+  a.add(24);
+ System.out.println(a);
+ a.add(38);
+  System.out.println(a);
+ System.out.println(a.get(1));
+  System.out.println(a.size());
+  a.add(0, 12);
+  System.out.println(a);
+ System.out.println( a.contains(12));
+  System.out.println(a.indexOf(38));
+	}
+
+}
